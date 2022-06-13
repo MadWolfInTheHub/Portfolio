dc26c321071d531b98b119929346d79328d02d17
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-06-13 15:54:00 +0800

    test

--- components/ContactInfo.jsx
+++ components/ContactInfo.jsx
@@ -1,13 +1,11 @@
-import styles from '../styles/Home.module.css'
-
 export default function ContactInfo({ contactInfo }) {
   const { link, contactType, icon } = contactInfo;
   return (
     <>
-      <a className={styles.logo_text} href={`${link}`}>
-        <span className={styles.logo}>
+      <a href={`${link}`}>
+        <div>
           <i className={`${icon}`}/>
-        </span>
+        </div>
         {`${contactType}`}
       </a>
     </>
