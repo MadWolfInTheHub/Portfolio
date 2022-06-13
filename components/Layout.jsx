58998bb96cbd668ed6f6ea8dbabb79629b95eb8b
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-06-13 16:59:19 +0800

    test

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,9 +4,9 @@ import Head from 'next/head'
 export default function Layuot({ info }) {
   return(
     <Head>
-    <title>Serhii's {info}</title>
-    <meta name="description" content="My CV" />
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
+      <title>Serhii's {info}</title>
+      <meta name="description" content="My CV" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
   )
 };
