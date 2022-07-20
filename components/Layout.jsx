3ca261c79221735d7b48ad25c5b447f713947d3e
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-07-20 16:43:44 +0800

    test

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,6 +7,12 @@ export default function Layuot({ info }) {
       <title>{name} {info}</title>
       <meta name="description" content="My CV" />
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+        integrity="sha256-WAgYcAck1C1/zEl5sBl5cfyhxtLgKGdpI3oKyJffVRI="
+        crossOrigin="anonymous"
+      />
     </Head>
   )
 };
